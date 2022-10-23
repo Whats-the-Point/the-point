48855e55d8468cd218388b565945cb0ef3611e84
Joao Botelho <joao.botelho@remote.com>
2022-10-24 00:39:58 +0100

    more specific error message

--- lib/the_point/users/services/submit_new_friendship.ex
+++ lib/the_point/users/services/submit_new_friendship.ex
@@ -7,15 +7,33 @@ defmodule ThePoint.Users.Services.SubmitNewFriendship do
 
   def call(requester_id, addressee_short_slug) do
     with addressee <- Users.get_user_by_short_slug(addressee_short_slug),
-         false <- Users.user_blocked_me?(requester_id, addressee.id),
-         false <- Users.exists_reverse_friendship?(requester_id, addressee.id) do
+         :ok <- is_requester_blocked_by_addressee(requester_id, addressee.id),
+         :ok <- validate_no_reverse_friendship(requester_id, addressee.id) do
       Users.create_friendship(%{requester_id: requester_id, addressee_id: addressee.id})
     else
       nil ->
         {:error, :not_found}
 
+      {:error, message} ->
+        {:error, 422, message}
+    end
+  end
+
+  defp is_requester_blocked_by_addressee(requester_id, addressee_id) do
+    case Users.user_blocked_me?(requester_id, addressee_id) do
+      false ->
+        :ok
       true ->
-        {:error, 422, "friendship already exists or you were blocked by this user."}
+        {:error, "you are blocked by this user"}
+    end
+  end
+
+  defp validate_no_reverse_friendship(requester_id, addressee_id) do
+    case Users.exists_reverse_friendship?(requester_id, addressee_id) do
+      false ->
+        :ok
+      true ->
+        {:error, "friendship already exists"}
     end
   end
 end
