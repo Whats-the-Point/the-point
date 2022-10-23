10c96027f9321a28800f01d04ab96eee9d7cde52
Joao Botelho <joao.botelho@remote.com>
2022-10-23 03:54:51 +0100

    basic methods for user and friendship manipulation

--- lib/the_point/users.ex
+++ lib/the_point/users.ex
@@ -2,12 +2,38 @@ defmodule ThePoint.Users do
   @moduledoc """
   The Users context
   """
+  import Ecto.Query
 
   alias ThePoint.Repo
   alias ThePoint.Users.User
+  alias ThePoint.Friendships.Friendship
 
-  @spec get_user(integer()) :: User.t() | nil
+  @doc """
+  Returns the list of users.
+
+  ## Examples
+
+      iex> list_users()
+      [%User{}, ...]
+
+  """
+  def list_users do
+    Repo.all(User)
+  end
+
+  def list_active_users do
+    Repo.all(from(user in User, where: user.status == :active))
+  end
+
   def get_user(id), do: Repo.get(User, id)
+
+  def get_user_by_short_slug(short_slug), do: Repo.get_by(User, %{short_slug: short_slug})
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
 
   @doc """
   Updates a User.
@@ -25,5 +51,28 @@ defmodule ThePoint.Users do
     user
     |> User.changeset(attrs)
     |> Repo.update()
+  end
+
+  def create_friendship(attrs \\ %{}) do
+    %Friendship{}
+    |> Friendship.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def exists_reverse_friendship?(requester_id, addressee_id) do
+    query =
+      from(fs in Friendship,
+        where: fs.requester_id == ^addressee_id and fs.addressee_id == ^requester_id
+      )
+
+    Repo.exists?(query)
+  end
+
+  @spec get_user_friendships_by_id(any) :: any
+  def get_user_friendships_by_id(id) do
+    query =
+      from(user in User, where: user.id == ^id, preload: [:friendships, :reverse_friendships])
+
+    Repo.all(query)
   end
 end
