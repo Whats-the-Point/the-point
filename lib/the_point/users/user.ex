e4fff115651312a9e1f6635b4d095cf2a4eced7f
Joao Botelho <joao.botelho@remote.com>
2022-11-13 23:24:08 +0000

    change minimum length of username

--- lib/the_point/users/user.ex
+++ lib/the_point/users/user.ex
@@ -38,7 +38,7 @@ defmodule ThePoint.Users.User do
     |> validate_required([:username, :name])
     |> unique_constraint(:username)
     |> validate_length(:name, count: :codepoints, max: 255)
-    |> validate_length(:username, count: :codepoints, min: 8, max: 20)
+    |> validate_length(:username, count: :codepoints, min: 5, max: 20)
     |> validate_username()
   end
 
@@ -47,7 +47,7 @@ defmodule ThePoint.Users.User do
   2. Underscore and dot can't be at the end or start of a username (e.g _username / username_ / .username / username.).
   3. Underscore and dot can't be next to each other (e.g user_.name).
   4. Underscore or dot can't be used multiple times in a row (e.g user__name / user..name).
-  5. Number of characters must be between 8 to 20.
+  5. Number of characters must be between 5 to 20.
   """
   def validate_username(changeset) do
     if String.match?(get_field(changeset, :username), ~r/^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*$/i) do
