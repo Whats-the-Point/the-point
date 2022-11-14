ec6e7f999e7b450ef88f7593b879bbddf2aa7f77
Joao Botelho <joao.botelho@remote.com>
2022-11-14 23:35:29 +0000

    callback google page

--- frontend/src/pages/authentication/CallbackGoogle.tsx
+++ frontend/src/pages/authentication/CallbackGoogle.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
