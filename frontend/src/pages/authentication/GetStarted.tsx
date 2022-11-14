e705f9d3e4a1751cf5453e8f26b715cc0a95a09f
Joao Botelho <joao.botelho@remote.com>
2022-11-14 23:35:54 +0000

    sign up page

--- frontend/src/pages/authentication/GetStarted.tsx
+++ frontend/src/pages/authentication/GetStarted.tsx
@@ -1,28 +1,31 @@
-import { useState, useRef } from 'react';
-import GoogleButton from 'react-google-button';
+import GoogleButton from '../../components/button/GoogleButton';
 import axios from 'axios';
+import whats_the_point from '../../assets/whats_the_point.svg';
+import './getStarted.css';
+import { useNavigate } from 'react-router-dom';
 
 const GetStarted: React.FC = () => {
-    const errRef = useRef<null | HTMLParagraphElement>(null);
-
-    const [state, setState] = useState<string>('');
-    const [authLink, setAuthLink] = useState<string>('');
-    const [errMsg, setErrMsg] = useState<string>('');
-    const [success, setSucess] = useState<boolean>(false);
-
-
+    const navigate = useNavigate()
     const loginwithGoogle = (event: React.MouseEvent<HTMLDivElement>) => {
         axios.get("/api/v1/auth/google/new").then((response) => {
             window.location.replace(response.data.url);
         });
     }
 
+    const goBack = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        navigate("/");
+    }
+
     return (
-        <section>
-            <p ref={errRef} className={errMsg ? "errMsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-            <h1>Sign In</h1>
-            <GoogleButton onClick={loginwithGoogle}/>
-        </section>
+        <div className='main-section'>
+            <div className='content-section'>
+                <img src={whats_the_point} alt="Whats the point" />
+                <h2>Hey, there</h2>
+                <body>Welcome to your personal scoreboard online. Invite your friends and keep track of your scores.</body>
+                <GoogleButton onClick={loginwithGoogle} />
+                <a onClick={goBack}>Go back</a>
+            </div>
+        </div>
     );
 }
 
