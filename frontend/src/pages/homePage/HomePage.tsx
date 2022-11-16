3c6adab00a07869a6205f480d848238772c38bcf
Joao Botelho <joao.botelho@remote.com>
2022-11-16 14:14:57 +0000

    setup page for video

--- frontend/src/pages/homePage/HomePage.tsx
+++ frontend/src/pages/homePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import Button from "../../components/button/Button";
 import { useNavigate } from 'react-router-dom';
-import whats_the_point from '../../assets/whats_the_point.svg'
+import whatsThePoint from '../../assets/whats_the_point.svg'
 import "./homePage.css"
 
 const HomePage: React.FC = () => {
@@ -8,14 +8,16 @@ const HomePage: React.FC = () => {
     const navigate = useNavigate();
 
     return (
-        <div>
+        <div className="main-home-page">
             <div className="footer">
-                <img src={whats_the_point} alt="Whats the point" />
+                <img src={whatsThePoint} alt="Whats the point" />
                 <Button onClick={() => navigate("/get-started")}>Get Started</Button>
             </div>
-            <h1 className="title">
-                Your<span style={style}> &nbsp;Scoreboard&nbsp;</span> online.
-            </h1>
+            <div>
+                <h1 className="title">
+                    Your<span style={style}> &nbsp;Scoreboard&nbsp;</span> online.
+                </h1>
+            </div>
         </div>
     );
 }
